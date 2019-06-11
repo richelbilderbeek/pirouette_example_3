8d9555844d789627052d27e06c7bd1eca638ea54
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-11 10:55:25 +0200

    Clean script [skip ci]

--- scripts/install_pirouette.sh
+++ scripts/install_pirouette.sh
@@ -7,6 +7,7 @@
 # 
 # sbatch install_pirouette.sh
 #
+#
 #SBATCH --time=1:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -14,12 +15,10 @@
 #SBATCH --mem=1G
 #SBATCH --job-name=install_pirouette
 #SBATCH --output=install_pirouette.log
-#module load GCCcore/4.9.3 
-#module load XZ/5.2.2-foss-2016a
 module load R
 Rscript -e 'devtools::install_github("ropensci/mauricer")'
 Rscript -e 'devtools::install_github("ropensci/babette")'
 Rscript -e 'devtools::install_github("richelbilderbeek/pirouette")'
 Rscript -e 'devtools::install_github("richelbilderbeek/becosys")'
-Rscript -e 'beastier::install_beast2()'
-Rscript -e 'mauricer::install_beast2_pkg("NS")' || true
+Rscript -e 'if (!beastier::is_beast2_installed()) beastier::install_beast2()'
+Rscript -e 'if (!mauricer::is_beast2_pkg_installed("NS")) mauricer::install_beast2_pkg("NS")'
