65629148c52f58e0cc91af22a348470f808980a5
richelbilderbeek <richel@richelbilderbeek.nl>
2019-11-16 14:26:37 +0100

    Use mutation rate of 0.1, as in pirouette article

--- example_3.R
+++ example_3.R
@@ -4,26 +4,6 @@
 
 # Set the RNG seed
 rng_seed <- 314
-args <- commandArgs(trailingOnly = TRUE)
-if (length(args) == 1) {
-  arg <- suppressWarnings(as.numeric(args[1]))
-  if (is.na(arg)) {
-    stop(
-      "Please supply a numerical value for the RNG seed. \n",
-      "Actual value: ", args[1]
-    )
-  }
-  rng_seed <- arg
-  if (rng_seed < 1) {
-    stop("Please supply an RNG seed with a positive non-zero value")
-  }
-}
-if (length(args) > 1) {
-  stop(
-    "Please supply only 1 argument for the RNG seed. \n",
-    "Number of arguments given: ", length(args) - 1
-  )
-}
 
 library(pirouette)
 suppressMessages(library(ggplot2))
@@ -41,10 +21,13 @@ phylogeny  <- ape::read.tree(
 )
 
 alignment_params <- create_alignment_params(
+  sim_true_alignment_fun = get_sim_true_alignment_with_std_site_model_fun(
+    mutation_rate = 0.1,
+    site_model = beautier::create_jc69_site_model()
+  ),
   root_sequence = create_blocked_dna(length = 1000),
   rng_seed = rng_seed
 )
-
 
 # JC69, strict, Yule
 generative_experiment <- create_gen_experiment()
