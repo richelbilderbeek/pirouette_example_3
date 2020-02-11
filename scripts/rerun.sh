50c9a7d53ecde871a313a8d15198920a4c085282
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-11 09:26:27 +0100

    Add Peregrine directives to bash script [skip ci]

--- scripts/rerun.sh
+++ scripts/rerun.sh
@@ -7,6 +7,16 @@
 #   ./scripts/rerun.sh
 #
 #
+#
+#SBATCH --partition=gelifes
+#SBATCH --time=10:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=10G
+#SBATCH --job-name=run_example_3
+#SBATCH --output=run_example_3.log
+#
 rm -rf example_3_314
 Rscript example_3.R
 
